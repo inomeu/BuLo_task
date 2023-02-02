08429730481bfad9f4765047d3fffa05c93f0434
Mei Inoue <inomei.0314@gmail.com>
2023-02-02 15:03:25 +0900

    First commit

---
+++ BuLo_task/BuLo_task/BuLo_taskApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuLo_taskApp.swift
+//  BuLo_task
+//
+//  Created by inoue mei on 2023/02/02.
+//
+
+import SwiftUI
+
+@main
+struct BuLo_taskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
