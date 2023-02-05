f1a3870663bf06ed2293ada3b436805df6cfdded
Mei Inoue <inomei.0314@gmail.com>
2023-02-05 18:18:13 +0900

    [Update] Update MapMarker

--- BuLo_task/BuLo_task/DataModel.swift
+++ BuLo_task/BuLo_task/DataModel.swift
@@ -15,8 +15,7 @@ class ViewController: UIViewController {
         let url = URL(string: "https://api.kantacky.com/muji/store")!
         var request = URLRequest(url: url)
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.allHTTPHeaderFields = ["Token": "ZeVlaZVIy0O6FhVaJpH-PbjNlyz5ymdE4o18QS4FWzlKEDwhTqZ-jAiypaZ9DtcbEpQuBKXWyEr-FIEDQIRpm67JxWBgQhzmBh7loF_Umgnmf1dvq7W5TLKWdmXJrctFSWbEGxZ6oM3pA-eC8FfB5HrEPDuWFH1jXgXzcg6yjrwOjkCg-lBTUCiE-BY66YL-80A4ICa8r0mVvuFJnDXPbGq9B_MivNgzFSBP2n3PPPkg7ZgyrX4YNeGikK9rn0XT"]                
-        //request.httpMethod = "POST"
+        request.allHTTPHeaderFields = ["AccessToken": "ZeVlaZVIy0O6FhVaJpH-PbjNlyz5ymdE4o18QS4FWzlKEDwhTqZ-jAiypaZ9DtcbEpQuBKXWyEr-FIEDQIRpm67JxWBgQhzmBh7loF_Umgnmf1dvq7W5TLKWdmXJrctFSWbEGxZ6oM3pA-eC8FfB5HrEPDuWFH1jXgXzcg6yjrwOjkCg-lBTUCiE-BY66YL-80A4ICa8r0mVvuFJnDXPbGq9B_MivNgzFSBP2n3PPPkg7ZgyrX4YNeGikK9rn0XT"]                
         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
             guard let data = data else { return }
             do {
--- BuLo_task/BuLo_task/MapView.swift
+++ BuLo_task/BuLo_task/MapView.swift
@@ -10,30 +10,29 @@ import MapKit
 
 struct MapView: View {
     @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 41.789656, longitude: 140.751924) ,
-        latitudinalMeters: 900,
-        longitudinalMeters: 900 )
-    //                                                   span: MKCoordinateSpan(latitudeDelta: 5.0, longitudeDelta: 5.0))
+                                                   latitudinalMeters: 900,
+                                                   longitudinalMeters: 900 )
     
     let place : IdentifiablePlace
     
-    var body:some View {
+    var body: some View {
         Map(coordinateRegion: $region,
-            interactionModes: .pan,
-            showsUserLocation: false,
             annotationItems: [place])
-        { place in MapMarker(coordinate: place.location, tint: Color.blue)
-            //            MapAnnotation(coordinate: place.location) {
-            //                Image(systemName: "tortoise.fill")
-            //                    .foregroundColor(Color(UIColor.systemBackground))
-            //                    .padding()
-            //                    .background(Color.orange.cornerRadius(10))
+        { place in
+            //            MapMarker(coordinate: place.location, tint: Color.blue)
+            MapAnnotation(coordinate: place.location) {
+                Image(systemName: "tortoise.fill")
+                    .foregroundColor(Color(UIColor.systemBackground))
+                    .padding()
+                    .background(Color.orange.cornerRadius(10))
+            }
         }
     }
-}
-struct MapView_Previews: PreviewProvider {
-    static var previews: some View {
-        MapView(place: IdentifiablePlace(lat: 41.789656, long: 140.751924))
+    struct MapView_Previews: PreviewProvider {
+        static var previews: some View {
+            MapView(place: IdentifiablePlace(lat: 41.789656, long: 140.751924))
+        }
     }
+    
+    
 }
-
-
